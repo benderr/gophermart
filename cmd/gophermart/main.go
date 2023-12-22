41db6dbe5ec799b5f5afda777a3e747fb4edcff5
Ильшат Гайфуллин <ilshat.gayfullin@sbermarket.ru>
2023-12-22 10:08:30 +0500

    [review] Remove ds store

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -2,26 +2,12 @@ package main
 
 import (
 	"context"
-	"net/http"
 
 	"github.com/benderr/gophermart/internal/app"
 	"github.com/benderr/gophermart/internal/config"
 
 	"github.com/benderr/gophermart/internal/logger"
-	"github.com/go-playground/validator"
-	"github.com/labstack/echo/v4"
 )
-
-type CustomValidator struct {
-	validator *validator.Validate
-}
-
-func (cv *CustomValidator) Validate(i interface{}) error {
-	if err := cv.validator.Struct(i); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
-	return nil
-}
 
 func main() {
 	conf := config.MustLoad()
